6fa57a7482a41fac41f5f8f28391a85fc5f3c98f
Helder Berto <helder.burato@gmail.com>
2023-06-27 09:48:46 +0000

    Add Censored + HitCounter

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { readFile, writeFile } from "../helpers/file-helpers";
-
-const DB_PATH = '/src/database.json'
+import Censored from "../components/Censored";
+import HitCounter from "../components/HitCounter/HitCounter";
 
 function Home() {
-  let { hits } = JSON.parse(readFile(DB_PATH))
-  
-  hits += 1
-
-  writeFile(DB_PATH, JSON.stringify({ hits }))
-
   return (
     <main>
       <h1>Welcome</h1>
-      <p>You are visitor number {hits}</p>
+      <p>You are visitor number <Censored><HitCounter /></Censored>.</p>
     </main>
   );
 }
